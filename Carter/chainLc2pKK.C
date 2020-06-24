648267a19659b77b44322df949b4621a57db434b
Carter <eikenbcr@mail.uc.edu>
2020-06-24 14:57:57 -0400

    Folder

---
+++ Carter/chainLc2pKK.C
@@ -0,0 +1,10 @@
+// TChains Four Ntuples from 2011/2012
+// Used With "root -l chainLc2pKK.C"
+
+{
+TChain chain("TupleLbToLcmu_LcTopKK/DecayTree");
+chain.Add("/share/lazy/Lc2pKK/2011/Lc2pXX.2011.MagDown.root");
+chain.Add("/share/lazy/Lc2pKK/2011/Lc2pXX.2011.MagUp.root");
+chain.Add("/share/lazy/Lc2pKK/2012/Lc2pXX.2012.MagDown.root");
+chain.Add("/share/lazy/Lc2pKK/2012/Lc2pXX.2012.MagUp.root");
+}
