142aab076b2fa8cc1e5a2f3e90104c1e50e3b11f
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-06-24 14:56:46 -0400

    Add Code

--- Carter/chainLc2pKKRun2.C
+++ Carter/chainLc2pKKRun2.C
@@ -1,1 +1,7 @@
-tt
+// TChains Four Ntuples from 2017
+// Used With "root -l chainLc2pKKRun2.C"
+
+{
+TChain chain("Lc2pKKTuple/DecayTree");
+chain.Add("/share/lazy/Lc2pKK/2017/LcTopKKNew11.root");
+}
