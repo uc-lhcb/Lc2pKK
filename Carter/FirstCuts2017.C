c1dd799aee60364376716c968f343f2eef9de1da
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-06-25 19:47:22 -0400

    LaTeX Output

--- Carter/FirstCuts2017.C
+++ Carter/FirstCuts2017.C
@@ -272,9 +272,6 @@ PullPlotMid->Draw("AB");
       c1->Write("Lc Mass - Mid");
 
 ex1->cd();
-   TLatex Tl;
-   Tl.SetTextAlign(12);
-   Tl.SetTextSize(0.04);
    Tl.DrawLatex(0.1,0.8,Form("Number of Signal Entries: %f Events", GaussianMid->GetParameter(0)));
    Tl.DrawLatex(0.1,0.7,Form("Error: %f Events", GaussianMid->GetParError(0)));
    Tl.DrawLatex(0.1,0.5,Form("Mean Value: %f MeV", GaussianMid->GetParameter(1)));
@@ -364,9 +361,6 @@ Tightcount3 += 1;
      c1->Write("Lc Mass - Tight");
 
 ex1->cd();
-   TLatex Tl;
-   Tl.SetTextAlign(12);
-   Tl.SetTextSize(0.04);
    Tl.DrawLatex(0.1,0.8,Form("Number of Signal Entries: %f Events", GaussianTight->GetParameter(0)));
    Tl.DrawLatex(0.1,0.7,Form("Error: %f Events", GaussianTight->GetParError(0)));
    Tl.DrawLatex(0.1,0.5,Form("Mean Value: %f MeV", GaussianTight->GetParameter(1)));
