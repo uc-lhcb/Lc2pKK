848ca568974aecb2b9670efd6a13d093e75d7d8c
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-06-25 16:02:30 -0400

    Update FirstCuts2017.C

--- Carter/FirstCuts2017.C
+++ Carter/FirstCuts2017.C
@@ -1,4 +1,4 @@
-// Similar Analysis to 'FirstCuts.C' but made specifically for Run 2 Data
+//First Cuts with Pull Plot
 
 #define FirstCuts2017_cxx
 #include "FirstCuts2017.h"
@@ -57,7 +57,7 @@ Bool_t FirstCuts2017::Process(Long64_t entry)
    bool LooseCuts = (
       (((*Kminus_ProbNNk)*(*Kplus_ProbNNk)*(*Proton_ProbNNp)) > 0.7)
    );
-   
+
    bool MidCuts = (
       (((*Kminus_ProbNNk)*(*Kplus_ProbNNk)*(*Proton_ProbNNp)) > 0.8)
    );
@@ -100,28 +100,197 @@ void FirstCuts2017::Terminate()
   TString totalStr;
   TString deltaTotalStr;
 
-  TF1 *Gaussian2MeV = new TF1("Gaussian2MeV",fit2MeV_Gaussian,2100.,2500.,5);
-  Gaussian2MeV->SetParameter(0,400.);
-  Gaussian2MeV->SetParameter(1,2286);
-  Gaussian2MeV->SetParameter(2, 6);
-  Gaussian2MeV->SetParLimits(2, 0.,20.);
-  Gaussian2MeV->SetParameter(3, 0.);
-  Gaussian2MeV->SetParameter(4, 0.);
+double Pullx[75];
+TPad *pad1 = new TPad("pad1","pad1",0,0.33,1,1);
+TPad *pad2 = new TPad("pad2","pad2",0,0,1,0.33);
+pad2->SetTopMargin(0.03030303);
+pad1->Draw();
+pad2->Draw();
 
-  MassHistLoose->SetMinimum(0);
-  MassHistLoose->Fit("Gaussian2MeV");
+/////////////////////////////
 
+TF1 *GaussianLoose = new TF1("GaussianLoose",fit2MeV_Gaussian,2200.,2400.,5);
+GaussianLoose->SetParameter(0,400.);
+GaussianLoose->SetParameter(1,2287);
+GaussianLoose->SetParameter(2, 6);
+GaussianLoose->SetParLimits(2, 0.,20.);
+GaussianLoose->SetParameter(3, 0.);
+GaussianLoose->SetParameter(4, 0.);
+
+pad1->cd();
+MassHistLoose->SetMinimum(0);
+MassHistLoose->Fit("GaussianLoose");
+
+int BinHeightLoose[75];
+int FitHeightLoose[75];
+double PullLoose[75];
+
+double Loosecount1 = 0;
+double Loosecount2 = 0;
+double Loosecount3 = 0;
+
+for (int bin = 0; bin < 75; bin++){
+BinHeightLoose[bin] = MassHistLoose->GetBinContent(bin + 1);
+Pullx[bin] = (bin + 1);
+int xvalue = 2210 + 2*(bin);
+FitHeightLoose[bin] = round(GaussianLoose->Eval(xvalue));
+PullLoose[bin] = (BinHeightLoose[bin] - FitHeightLoose[bin])/TMath::Sqrt(FitHeightLoose[bin]);
+
+if (PullLoose[bin] > -1 && PullLoose[bin] < 1){
+  Loosecount1 += 1;
+}
+
+if (PullLoose[bin] > -2 && PullLoose[bin] < 2){
+  Loosecount2 += 1;
+}
+
+if (PullLoose[bin] > -3 && PullLoose[bin] < 3){
+  Loosecount3 += 1;
+}
+}
+
+pad2->cd();
+TGraph* PullPlotLoose = new TGraph(75, Pullx, PullLoose);
+PullPlotLoose->GetXaxis()->SetLimits(0,75);
+PullPlotLoose->GetXaxis()->SetTickLength(0.);
+PullPlotLoose->GetYaxis()->SetTickLength(0.);
+PullPlotLoose->SetFillColor(38);
+PullPlotLoose->GetYaxis()->SetTitle("Pull");
+PullPlotLoose->GetYaxis()->CenterTitle();
+PullPlotLoose->GetYaxis()->SetTitleSize(0.10);
+PullPlotLoose->GetYaxis()->SetTitleOffset(0.2);
+PullPlotLoose->GetXaxis()->SetLabelSize(0);
+PullPlotLoose->GetYaxis()->SetLabelFont(42);
+PullPlotLoose->GetYaxis()->SetLabelSize(0.06);
+PullPlotLoose->SetTitle("");
+PullPlotLoose->SetMinimum(-5);
+PullPlotLoose->SetMaximum(5);
+PullPlotLoose->Draw("AB");
       c1->Write("Lc Mass - Loose");
 
+///////////////////////////////////////////////////////
+
+TF1 *GaussianMid = new TF1("GaussianMid",fit2MeV_Gaussian,2200.,2400.,5);
+GaussianMid->SetParameter(0,400.);
+GaussianMid->SetParameter(1,2287);
+GaussianMid->SetParameter(2, 6);
+GaussianMid->SetParLimits(2, 0.,20.);
+GaussianMid->SetParameter(3, 0.);
+GaussianMid->SetParameter(4, 0.);
+
+  pad1->cd();
   MassHistMid->SetMinimum(0);
-  MassHistMid->Fit("Gaussian2MeV");
+  MassHistMid->Fit("GaussianMid");
 
-      c1->Write("Lc Mass - Original");
+  int BinHeightMid[75];
+  int FitHeightMid[75];
+  double PullMid[75];
 
-  MassHistTight->SetMinimum(0);
-  MassHistTight->Fit("Gaussian2MeV");
+   Midcount1 = 0;
+   Midcount2 = 0;
+   Midcount3 = 0;
 
+  for (int bin = 0; bin < 75; bin++){
+  BinHeightMid[bin] = MassHistMid->GetBinContent(bin + 1);
+  int xvalue = 2210 + 2*(bin);
+  FitHeightMid[bin] = round(GaussianMid->Eval(xvalue));
+  PullMid[bin] = (BinHeightMid[bin] - FitHeightMid[bin])/TMath::Sqrt(FitHeightMid[bin]);
+
+  if (PullMid[bin] > -1 && PullMid[bin] < 1){
+  Midcount1 += 1;
+  }
+
+  if (PullMid[bin] > -2 && PullMid[bin] < 2){
+  Midcount2 += 1;
+  }
+
+  if (PullMid[bin] > -3 && PullMid[bin] < 3){
+  Midcount3 += 1;
+  }
+  }
+
+  pad2->cd();
+TGraph* PullPlotMid = new TGraph(75, Pullx, PullMid);
+PullPlotMid->GetXaxis()->SetLimits(0,75);
+PullPlotMid->GetXaxis()->SetTickLength(0.);
+PullPlotMid->GetYaxis()->SetTickLength(0.);
+PullPlotMid->SetFillColor(38);
+PullPlotMid->GetYaxis()->SetTitle("Pull");
+PullPlotMid->GetYaxis()->CenterTitle();
+PullPlotMid->GetYaxis()->SetTitleSize(0.10);
+PullPlotMid->GetYaxis()->SetTitleOffset(0.2);
+PullPlotMid->GetXaxis()->SetLabelSize(0);
+PullPlotMid->GetYaxis()->SetLabelFont(42);
+PullPlotMid->GetYaxis()->SetLabelSize(0.06);
+PullPlotMid->SetTitle("");
+PullPlotMid->SetMinimum(-5);
+PullPlotMid->SetMaximum(5);
+PullPlotMid->Draw("AB");
+c1->Write("DG w/ 1 Mu & 2 Totals");
+
+      c1->Write("Lc Mass - Mid");
+
+//////////////////////////////////////////////////////
+
+TF1 *GaussianTight = new TF1("GaussianTight",fit2MeV_Gaussian,2200.,2400.,5);
+GaussianTight->SetParameter(0,400.);
+GaussianTight->SetParameter(1,2287);
+GaussianTight->SetParameter(2, 6);
+GaussianTight->SetParLimits(2, 0.,20.);
+GaussianTight->SetParameter(3, 0.);
+GaussianTight->SetParameter(4, 0.);
+
+pad1->cd();
+MassHistTight->SetMinimum(0);
+MassHistTight->Fit("GaussianTight");
+
+int BinHeightTight[100];
+int FitHeightTight[100];
+double PullTight[100];
+
+ Tightcount1 = 0;
+ Tightcount2 = 0;
+ Tightcount3 = 0;
+
+for (int bin = 0; bin < 75; bin++){
+BinHeightTight[bin] = MassHistTight->GetBinContent(bin + 1);
+int xvalue = 2210 + 2*(bin);
+FitHeightTight[bin] = round(GaussianTight->Eval(xvalue));
+PullTight[bin] = (BinHeightTight[bin] - FitHeightTight[bin])/TMath::Sqrt(FitHeightTight[bin]);
+
+if (PullTight[bin] > -1 && PullTight[bin] < 1){
+Tightcount1 += 1;
+}
+
+if (PullTight[bin] > -2 && PullTight[bin] < 2){
+Tightcount2 += 1;
+}
+
+if (PullTight[bin] > -3 && PullTight[bin] < 3){
+Tightcount3 += 1;
+}
+}
      c1->Write("Lc Mass - Tight");
+     pad2->cd();
+     TGraph* PullPlotTight = new TGraph(75, Pullx, PullTight);
+     PullPlotTight->GetXaxis()->SetLimits(0,75);
+     PullPlotTight->GetXaxis()->SetTickLength(0.);
+     PullPlotTight->GetYaxis()->SetTickLength(0.);
+     PullPlotTight->SetFillColor(38);
+     PullPlotTight->GetYaxis()->SetTitle("Pull");
+     PullPlotTight->GetYaxis()->CenterTitle();
+     PullPlotTight->GetYaxis()->SetTitleSize(0.10);
+     PullPlotTight->GetYaxis()->SetTitleOffset(0.2);
+     PullPlotTight->GetXaxis()->SetLabelSize(0);
+     PullPlotTight->GetYaxis()->SetLabelFont(42);
+     PullPlotTight->GetYaxis()->SetLabelSize(0.06);
+     PullPlotTight->SetTitle("");
+     PullPlotTight->SetMinimum(-5);
+     PullPlotTight->SetMaximum(5);
+     PullPlotTight->Draw("AB");
+     c1->Write("Lc Mass - Tight");
+
+/////////////////////////////
 
         File->Close();
 }
