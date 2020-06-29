1063f3cfae990f086a07b4491b88dcbad5e676b0
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-06-29 12:09:15 -0400

    Create fit1MeV_Gaussian.C

---
+++ Carter/fit1MeV_Gaussian.C
@@ -0,0 +1,24 @@
+//Gaussian Function for 2 MeV bin width (used to fit data)
+
+#pragma once
+
+#include "TMath.h"
+
+Double_t fit2MeV_Gaussian(Double_t *v, Double_t *par) {
+
+
+    Double_t arg = 0;
+    if (par[2] != 0)
+        arg = (v[0] - par[1])/par[2];
+
+    Double_t fitval = par[0]*TMath::Exp(-0.5*arg*arg);
+
+    
+    fitval = (fitval*1.00)/(TMath::Sqrt(TMath::TwoPi())*par[2]);
+
+
+    fitval = fitval + par[3] + v[0]*par[4];
+
+ 
+    return fitval;
+}
