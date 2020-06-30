99438c91a600ec0b0d63612187b3af46eb1cb0ba
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-06-30 13:00:25 -0400

    Update DalitzPlot.C

--- Carter/DalitzPlot.C
+++ Carter/DalitzPlot.C
@@ -63,9 +63,10 @@ double M2_PKm  = (((E_P)+(E_Km))*((E_P)+(E_Km)) - ((P_P)+(P_Km))*((P_P)+(P_Km)))
  PKmMassHist->Fill(M2_PKm);
 
  bool Cuts = (
-    M2_KpKm < 1.1;
- && (*Kminus_ProbNNk)*(Kplus_ProbNNk)*(Proton_ProbNNp) > 0.9;
- )
+    M2_KpKm < 1.1
+ && (*Kminus_ProbNNk)*(Kplus_ProbNNk)*(Proton_ProbNNp) > 0.9
+ );
+   
    if (Cuts){
     MassHist->Fill(*Lcplus_M);
  }
