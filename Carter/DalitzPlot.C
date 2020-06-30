a083f9ee8a75a96e6c523afcce58a5275ee267fb
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-06-30 12:20:27 -0400

    Update DalitzPlot.C

--- Carter/DalitzPlot.C
+++ Carter/DalitzPlot.C
@@ -18,7 +18,7 @@ void DalitzPlot::Begin(TTree * /*tree*/)
          DalitzPlot = new TH2D("Dalitz Plot", "Dalitz Plot of Lc->pKK Decay", 100, 0.7, 4.2, 100, 0.25, 1.0);
          DalitzPlot->GetXaxis()->SetTitle("m^{2}(K^{-}K^{+})[GeV^{2}/c^{4}]");
          DalitzPlot->GetYaxis()->SetTitle("m^{2}(pK^{-})[GeV^{2}/c^{4}]");
-         DalitzPlot->GetZaxis()->SetTitle(Entries);
+         DalitzPlot->GetZaxis()->SetTitle("Events");
                                           
          KpKmMassHist = new TH1D("M^{2} [GeV^{2}/c^{4}]", "Kplus & Pplus Invariant Mass Combination", 100, 1, 2);
          KpKmMassHist->GetXaxis()->SetTitle("m^{2}(K^{-}K^{+})[GeV^{2}/c^{4}]");                    
@@ -47,12 +47,12 @@ Bool_t DalitzPlot::Process(Long64_t entry)
   double M_Kp = *Kplus_M;
   double M_Km = *Kminus_M;
      
-  double E_P  = TMATH::Sqrt((P_P)*(P_P)+(P_M)*(P_M));
-  double E_Kp = TMATH::Sqrt((Kp_P)*(Kp_P)+(Kp_M)*(Kp_M));
-  double E_Km = TMATH::Sqrt((Km_P)*(Km_P)+(Km_M)*(Km_M));
+  double E_P  = TMath::Sqrt((P_P)*(P_P)+(M_P)*(M_P));
+  double E_Kp = TMath::Sqrt((P_Kp)*(P_Kp)+(M_Kp)*(M_Kp));
+  double E_Km = TMath::Sqrt((P_Km)*(P_Km)+(M_Km)*(M_Km));
    
-double M2_KpKm = (((E_Kp)+(E_Km))*((E_Kp)+(E_Km)) - ((Kp_P)+(Km_P))*((Kp_P)+(Km_P)))/(1000*1000);
-double M2_PKm  = (((E_P)+(E_Km))*((E_P)+(E_Km)) - ((P_P)+(Km_P))*((P_P)+(Km_P)))/(1000*1000);
+double M2_KpKm = (((E_Kp)+(E_Km))*((E_Kp)+(E_Km)) - ((P_Kp)+(P_Km))*((P_Kp)+(P_Km)))/(1000*1000);
+double M2_PKm  = (((E_P)+(E_Km))*((E_P)+(E_Km)) - ((P_P)+(P_Km))*((P_P)+(P_Km)))/(1000*1000);
    
  KpKmMassHist->Fill(M2_KpKm);
    
