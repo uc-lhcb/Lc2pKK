9181ab54f8650f21fd68ed58f1a93cf941cc420f
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-07-02 15:44:57 -0400

    Update DalitzPlot.C

--- Carter/DalitzPlot.C
+++ Carter/DalitzPlot.C
@@ -15,6 +15,10 @@ TH1D * KpKmMassHist = nullptr;
 TH1D * PKmMassHist = nullptr;
 TH1D * PKpMassHist = nullptr;
 
+
+TH1D * MassHist = nullptr;
+TH1D * BSubHist = nullptr;
+
 //ROOT File Creation
 TFile * File = nullptr;
 
@@ -46,6 +50,15 @@ void DalitzPlot::Begin(TTree * /*tree*/)
          PKpMassHist->GetXaxis()->SetTitle("m^{2}(pK^{+})[GeV^{2}/c^{4}]");                   
          PKpMassHist->GetYaxis()->SetTitle("Events");   
 
+   
+   MassHist = new TH1D("Mass [MeV]", "Lc->pKK - Lc Mass", 300, 2210, 2360);
+   MassHist->GetXaxis()->SetTitle("MeV");
+   MassHist->GetYaxis()->SetTitle("Events Per 2 MeV");
+   
+   BSubHist = new TH1D("Mass [MeV]", "Lc->pKK - Lc Mass", 50, 2275, 2300);
+   BSubHist->GetXaxis()->SetTitle("MeV");
+   BSubHist->GetYaxis()->SetTitle("Events Per 2 MeV");
+   
    //Output ROOT File
     File = new TFile("DalitzAnalysis.root", "RECREATE");
   gFile = File;
@@ -83,6 +96,11 @@ double M2_KpKm = ((((E_Kp)+(E_Km))*((E_Kp)+(E_Km))) - (((P_Kp)+(P_Km))*((P_Kp)+(
 double M2_PKm  = ((((E_P)+(E_Km))*((E_P)+(E_Km))) - (((P_P)+(P_Km))*((P_P)+(P_Km))))/(1000*1000);
 double M2_PKp  = ((((E_P)+(E_Kp))*((E_P)+(E_Kp))) - (((P_P)+(P_Kp))*((P_P)+(P_Kp))))/(1000*1000);
 
+ if ((*Kminus_ProbNNk)*(*Kplus_ProbNNk)*(*Proton_ProbNNp)) > 0.6){
+    MassHist->Fill(*Lcplus_M);
+    BsubHist->Fill(*Lcplus_M);
+ }  
+       
 //Filling Histogram
  KpKmMassHist->Fill(M2_KpKm);
  PKmMassHist->Fill(M2_PKm);
@@ -119,6 +137,35 @@ KpKmMassHist->Draw();
    
  PKpMassHist->Draw();
  c1->Write("P & Kp Mass");
-  
+ 
+   
+  Double_t sigma;
+  Double_t deltaSigma;
+  Double_t mu;
+  Double_t deltaMu;
+  Double_t total;
+  Double_t deltaTotal;
+  TString sigmaStr;
+  TString deltaSigmaStr;
+  TString muStr;
+  TString deltaMuStr;
+  TString totalStr;
+  TString deltaTotalStr;
+   
+TF1 *GaussianLooseHalfMeVSG = new TF1("GaussianLooseHalfMeVSG",fitHalfMeV_Gaussian,2100.,2500.,5);
+GaussianLooseHalfMeVSG->SetParameter(0,400.);
+GaussianLooseHalfMeVSG->SetParameter(1,2287);
+GaussianLooseHalfMeVSG->SetParameter(2, 6);
+GaussianLooseHalfMeVSG->SetParLimits(2, 0.,20.);
+GaussianLooseHalfMeVSG->SetParameter(3, 0.);
+GaussianLooseHalfMeVSG->SetParameter(4, 0.);
+   
+MassHist->SetMinimum(0);
+MassHist->Fit("GaussianLooseHalfMeVSG");  
+ c1->Write("Lc Mass"); 
+   
+BSubHist->Draw();
+c1->Write("Signal Events")  
+   
 File->Close();
 }
