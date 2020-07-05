445d05e91109d1304ed855c9619300cfb893fb12
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-07-05 17:36:04 -0400

    Update DalitzPlot.C

--- Carter/DalitzPlot.C
+++ Carter/DalitzPlot.C
@@ -1,9 +1,8 @@
 #define DalitzPlot_cxx
 #include "DalitzPlot.h"
 
-#include "fitHalfMeV_Gaussian.C"
+#include "DGOneMuOneTotalHalfMeV.C"
 
-//ROOT Libraries
 #include <TH1D.h>
 #include <TH2D.h>
 #include <TStyle.h>
@@ -11,7 +10,6 @@
 #include <TFile.h>
 #include <TF1.h>
 
-//Histogram Creation
 TH2D * DalitzPlotLc = nullptr;
 TH1D * KpKmMassHist = nullptr;
 TH1D * PKmMassHist = nullptr;
@@ -21,11 +19,10 @@ TH1D * PKpMassHist = nullptr;
 TH1D * MassHist = nullptr;
 TH1D * BSubHist = nullptr;
 
-//ROOT File Creation
 TFile * File = nullptr;
 
-//Canvas Creation
 TCanvas * c1 = nullptr;
+TCanvas * ex1 = nullptr;
 
 void DalitzPlot::Begin(TTree * /*tree*/)
 {
@@ -61,12 +58,12 @@ void DalitzPlot::Begin(TTree * /*tree*/)
    BSubHist->GetXaxis()->SetTitle("MeV");
    BSubHist->GetYaxis()->SetTitle("Events Per 2 MeV");
    
-   //Output ROOT File
     File = new TFile("DalitzAnalysis.root", "RECREATE");
   gFile = File;
 
-   //Defining Canvas
+
    c1 = new TCanvas("canvas", "Test Canvas");
+   ex1 = new TCanvas("ex1","Latex",500,600);
 }
 
 void DalitzPlot::SlaveBegin(TTree * /*tree*/)
@@ -111,7 +108,8 @@ double M2_PKp  = (KplusMass + ProtonMass)*(KplusMass + ProtonMass)/(1000*1000);
 //double M2_PKp  = ((((E_P)+(E_Kp))*((E_P)+(E_Kp))) - (((P_P)+(P_Kp))*((P_P)+(P_Kp))))/(1000*1000);
 
 bool Cut = (
-   ((*Kminus_ProbNNk)*(*Kplus_ProbNNk)*(*Proton_ProbNNp) > 0.6)
+     ((*Kminus_ProbNNk)*(*Kplus_ProbNNk)*(*Proton_ProbNNp) > 0.6)
+&&   ((*Kminus_ProbNNk)*(Kplus_ProbNNk) > 0.7))
    );
    
 bool SignalRegion = (
@@ -161,7 +159,15 @@ KpKmMassHist->Draw();
    
  PKpMassHist->Draw();
  c1->Write("P & Kp Mass");
- 
+    
+BSubHist->Draw();
+c1->Write("Signal Events");
+   
+TPad *pad1 = new TPad("pad1","pad1",0,0.33,1,1);
+TPad *pad2 = new TPad("pad2","pad2",0,0,1,0.33);
+pad2->SetTopMargin(0.03030303);
+pad1->Draw();
+pad2->Draw();
    
   Double_t sigma;
   Double_t deltaSigma;
@@ -176,20 +182,90 @@ KpKmMassHist->Draw();
   TString totalStr;
   TString deltaTotalStr;
    
-TF1 *GaussianLooseHalfMeVSG = new TF1("GaussianLooseHalfMeVSG",fitHalfMeV_Gaussian,2100.,2500.,5);
-GaussianLooseHalfMeVSG->SetParameter(0,400.);
-GaussianLooseHalfMeVSG->SetParameter(1,2287);
-GaussianLooseHalfMeVSG->SetParameter(2, 6);
-GaussianLooseHalfMeVSG->SetParLimits(2, 0.,20.);
-GaussianLooseHalfMeVSG->SetParameter(3, 0.);
-GaussianLooseHalfMeVSG->SetParameter(4, 0.);
-   
+TF1 *Gaussian = new TF1("Gaussian",DGOneMuOneTotalHalfMeV,2200.,2400.,7);
+Gaussian->SetParameter(0, 0.5);
+Gaussian->SetParameter(1, 10000);
+Gaussian->SetParameter(2, 2287.);
+Gaussian->SetParameter(3, 5);
+Gaussian->SetParameter(4, 5);
+Gaussian->SetParLimits(3, 0., 20.);
+Gaussian->SetParLimits(4, 0., 20.);
+Gaussian->SetParameter(5, 0.);
+Gaussian->SetParameter(6, 0.);
+
+pad1->cd();
 MassHist->SetMinimum(0);
-MassHist->Fit("GaussianLooseHalfMeVSG");  
- c1->Write("Lc Mass"); 
+MassHist->Fit("Gaussian");
+
+double PullxHalfMeV[300];
+int BinHeight[300];
+int FitHeight[300];
+double Pull[300];
+
+double count1 = 0;
+double count2 = 0;
+double count3 = 0;
+
+for (int bin = 0; bin < 300; bin++){
+BinHeight[bin] = MassHist->GetBinContent(bin + 1);
+Pullx[bin] = (bin + 1);
+int xvalue = 2210.25 + 0.5*(bin);
+FitHeight[bin] = round(Gaussian->Eval(xvalue));
+Pull[bin] = (BinHeightbin] - FitHeight[bin])/TMath::Sqrt(FitHeight[bin]);
+
+if (Pull[bin] > -1 && Pull[bin] < 1){
+  count1 += 1;
+}
+
+if (Pull[bin] > -2 && Pull[bin] < 2){
+  count2 += 1;
+}
+
+if (Pull[bin] > -3 && Pull[bin] < 3){
+  count3 += 1;
+}
+}
+
+pad2->cd();
+TGraph* PullPlot = new TGraph(300, Pullx, Pull);
+PullPlot->GetXaxis()->SetLimits(0.5,300.5);
+PullPlot->GetXaxis()->SetTickLength(0.);
+PullPlot->GetYaxis()->SetTickLength(0.);
+PullPlot->SetFillColor(38);
+PullPlot->GetYaxis()->SetTitle("Pull");
+PullPlot->GetYaxis()->CenterTitle();
+PullPlot->GetYaxis()->SetTitleSize(0.10);
+PullPlot->GetYaxis()->SetTitleOffset(0.2);
+PullPlot->GetXaxis()->SetLabelSize(0);
+PullPlot->GetYaxis()->SetLabelFont(42);
+PullPlot->GetYaxis()->SetLabelSize(0.06);
+PullPlot->SetTitle("");
+PullPlot->SetMinimum(-5);
+PullPlot->SetMaximum(5);
+PullPlot->Draw("AB");
+      c1->Write("Lc Mass");
+
+ex1->cd();
+ ex1->Clear();
+   TLatex Tl;
+   Tl.SetTextAlign(12);
+   Tl.SetTextSize(0.04);
+   Tl.DrawLatex(0.1,0.95,Form("Number of Signal Entries: %f Events", Gaussian->GetParameter(1)));
+   Tl.DrawLatex(0.1,0.9,Form("Error: %f Events", Gaussian->GetParError(1)));
+   Tl.DrawLatex(0.1,0.8,Form("Percentage of Events in First Gaussian: %f Events", Gaussian->GetParameter(0)));
+   Tl.DrawLatex(0.1,0.75,Form("Error: %f Events", Gaussian->GetParError(0)));
+   Tl.DrawLatex(0.1,0.65,Form("Mean Value: %f MeV", Gaussian->GetParameter(2)));
+   Tl.DrawLatex(0.1,0.6,Form("Error: %f MeV", Gaussian->GetParError(2)));
+   Tl.DrawLatex(0.1,0.5,Form("Sigma of First Gaussian: %f MeV", Gaussian->GetParameter(3)));
+   Tl.DrawLatex(0.1,0.45,Form("Error: %f MeV", Gaussian->GetParError(3)));
+   Tl.DrawLatex(0.1,0.35,Form("Sigma of Second Gaussian: %f MeV", Gaussian->GetParameter(4)));
+   Tl.DrawLatex(0.1,0.3,Form("Error: %f MeV", Gaussian->GetParError(4)));
+   Tl.DrawLatex(0.1,0.2,Form("Bins Between -1 & 1 %f Bins", count1));
+   Tl.DrawLatex(0.1,0.15,Form("Bins Between -2 & 2 %f Bins", count2));
+   Tl.DrawLatex(0.1,0.1,Form("Bins Between -3 & 3 %f Bins", count3));
    
-BSubHist->Draw();
-c1->Write("Signal Events");
+   ex1->Write("Fit Values");
+c1->cd();
    
 File->Close();
 }
