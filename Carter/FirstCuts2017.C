35157bc49a7c3540fc487b34e538338c3c02bdad
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-07-07 11:06:29 -0400

    Statistics

--- Carter/FirstCuts2017.C
+++ Carter/FirstCuts2017.C
@@ -25,6 +25,28 @@ TH1D * MassHistLoose1MeV = nullptr;
 TH1D * MassHistMid1MeV = nullptr;
 TH1D * MassHistTight1MeV = nullptr;
 
+TH1D * LcLowP = nullptr;
+TH1D * LcMidLowP = nullptr;
+TH1D * LcMidHighP = nullptr;
+TH1D * LcHighP = nullptr;
+TH1D * LcPDistribution = nullptr;
+
+TH1D * PolarityMagUp = nullptr;
+TH1D * PolarityMagDown = nullptr;
+
+TH1D * Particle = nullptr;
+TH1D * AntiParticle = nullptr;
+
+TGraphErrors * gLcP = nullptr;
+TGraphErrors * gXiP = nullptr;
+TGraphErrors * gPolarity = nullptr;
+TGraphErrors * gPID = nullptr;
+
+TGraphErrors * gLcPDG = nullptr;
+TGraphErrors * gXiPDG = nullptr;
+TGraphErrors * gPolarityDG = nullptr;
+TGraphErrors * gPIDDG = nullptr;
+
 TFile * File = nullptr;
 
 TCanvas * c1 = nullptr;
@@ -36,15 +58,15 @@ void FirstCuts2017::Begin(TTree * /*tree*/)
 
    MassHistLooseHalfMeV = new TH1D("Mass [MeV]", "Lc->pKK - Lc Mass", 300, 2210, 2360);
    MassHistLooseHalfMeV->GetXaxis()->SetTitle("MeV");
-   MassHistLooseHalfMeV->GetYaxis()->SetTitle("Events Per 2 MeV");
+   MassHistLooseHalfMeV->GetYaxis()->SetTitle("Events Per 1/2 MeV");
 
    MassHistMidHalfMeV = new TH1D("Mass [MeV]", "Lc->pKK - Lc Mass", 300, 2210, 2360);
    MassHistMidHalfMeV->GetXaxis()->SetTitle("MeV");
-   MassHistMidHalfMeV->GetYaxis()->SetTitle("Events Per 2 MeV");
+   MassHistMidHalfMeV->GetYaxis()->SetTitle("Events Per 1/2 MeV");
 
    MassHistTightHalfMeV = new TH1D("Mass [MeV]", "Lc->pKK - Lc Mass", 300, 2210, 2360);
    MassHistTightHalfMeV->GetXaxis()->SetTitle("MeV");
-   MassHistTightHalfMeV->GetYaxis()->SetTitle("Events Per 2 MeV");
+   MassHistTightHalfMeV->GetYaxis()->SetTitle("Events Per 1/2 MeV");
 
    MassHistLoose1MeV = new TH1D("Mass [MeV]", "Lc->pKK - Lc Mass", 150, 2210, 2360);
    MassHistLoose1MeV->GetXaxis()->SetTitle("MeV");
@@ -58,6 +80,42 @@ void FirstCuts2017::Begin(TTree * /*tree*/)
    MassHistTight1MeV->GetXaxis()->SetTitle("MeV");
    MassHistTight1MeV->GetYaxis()->SetTitle("Events Per 1 MeV");
 
+            LcLowP = new TH1D("Figures of Merit", "LambdaC Mass - Low LcP", 300, 2210, 2360);
+            LcLowP->GetXaxis()->SetTitle("MeV");
+            LcLowP->GetYaxis()->SetTitle("Events Per 1/2 MeV");
+
+            LcMidLowP = new TH1D("Figures of Merit", "LambdaC Mass - MidLow LcP", 300, 2210, 2360);
+            LcMidLowP->GetXaxis()->SetTitle("MeV");
+            LcMidLowP->GetYaxis()->SetTitle("Events Per 1/2 MeV");
+
+            LcMidHighP = new TH1D("Figures of Merit", "LambdaC Mass - MidHigh LcP", 300, 2210, 2360);
+            LcMidHighP->GetXaxis()->SetTitle("MeV");
+            LcMidHighP->GetYaxis()->SetTitle("Events Per 1/2 MeV");
+
+            LcHighP = new TH1D("Figures of Merit", "LambdaC Mass - High LcP", 300, 2210, 2360);
+            LcHighP->GetXaxis()->SetTitle("MeV");
+            LcHighP->GetYaxis()->SetTitle("Events Per 1/2 MeV");
+
+            LcPDistribution = new TH1D("Figures of Merit", "LcP Distribution After Cuts", 300, 0, 250000);
+            LcPDistribution->GetXaxis()->SetTitle("MeV");
+            LcPDistribution->GetYaxis()->SetTitle("Events Per 2500 MeV");
+   
+             PolarityMagDown = new TH1D("Figures of Merit", "LambdaC Mass - MagnetDown", 300, 2210, 2360);
+            PolarityMagDown->GetXaxis()->SetTitle("MeV");
+            PolarityMagDown->GetYaxis()->SetTitle("Events Per 1/2 MeV");
+
+            PolarityMagUp = new TH1D("Figures of Merit", "LambdaC Mass - MagnetUp", 300, 2210, 2360);
+            PolarityMagUp->GetXaxis()->SetTitle("MeV");
+            PolarityMagUp->GetYaxis()->SetTitle("Events Per 1/2 MeV");
+
+            Particle = new TH1D("Figures of Merit", "LambdaC Mass - LambdaC Baryon", 300, 2210, 2360);
+            Particle->GetXaxis()->SetTitle("MeV");
+            Particle->GetYaxis()->SetTitle("Events Per 1/2 MeV");
+
+            AntiParticle = new TH1D("Figures of Merit", "LambdaC Mass - LambdaC AntiBaryon", 300, 2210, 2360);
+            AntiParticle->GetXaxis()->SetTitle("MeV");
+            AntiParticle->GetYaxis()->SetTitle("Events Per 1/2 MeV");
+   
    File = new TFile("MassHist2017.root", "RECREATE");
   gFile = File;
 
@@ -111,8 +169,73 @@ Bool_t FirstCuts2017::Process(Long64_t entry)
    if (TightCuts){
      MassHistTightHalfMeV->Fill(*Lcplus_M);
      MassHistTight1MeV->Fill(*Lcplus_M);
+     LcPDistribution->Fill(*Lcplus_P);
    }
 
+ /////////////////////////////////////////  
+   
+     bool LcLowPCut = (
+     (*Lcplus_P < 50200.)
+   );
+
+   bool LcMidLowPCut = (
+     (*Lcplus_P > 50200. && *Lcplus_P < 65700.)
+   );
+
+   bool LcMidHighPCut = (
+     (*Lcplus_P > 65700. && *Lcplus_P < 87900.)
+   );
+
+   bool LcHighPCut = (
+     (*Lcplus_P > 87900.)
+   );
+
+   bool MagDown = (
+      (*Polarity < 0.)
+   );
+
+   bool MagUp = (
+      (*Polarity > 0.)
+   );
+
+   bool Particles = (
+      (*Lcplus_ID > 0.)
+   );
+
+   bool AntiParticles = (
+      (*Lcplus_ID < 0.)
+   );
+  
+ ///////////////////
+   
+ if (TightCuts && LcLowPCut){
+   LcLowP->Fill(*Lcplus_M);}
+  
+    if (TightCuts && LcMidLowPCut){
+   LcMidLowP->Fill(*Lcplus_M);}
+ 
+       if (TightCuts && LcMidHighPCut){
+   LcMidHighP->Fill(*Lcplus_M);}
+   
+          if (TightCuts && LcHighPCut){
+   LcHighP->Fill(*Lcplus_M);}
+   
+   ///////////////
+   
+  if (TightCuts && MagDown){
+   PolarityMagDown->Fill(*Lcplus_M);}
+  
+    if (TightCuts && MagUp){
+   PolarityMagUp->Fill(*Lcplus_M);}
+ 
+  /////////////// 
+   
+       if (TightCuts && Particles){
+   Particle->Fill(*Lcplus_M);}
+   
+          if (TightCuts && Antiparticles){
+   Antiparticle->Fill(*Lcplus_M);}  
+   
    return kTRUE;
 }
 
@@ -1588,5 +1711,183 @@ Tl.DrawLatex(0.1,0.02,Form("Bins Between -3 & 3 %f Bins", Tight1MeVDG2Mucount3))
    ex1->Write("Tight Fit Values/1MeVDG2Mu");
 c1->cd();
 /////////////////////////////
+   
+     TF1 *myLambdaFitSG = new TF1("myLambdaFitSG",fit2MeV_Gaussian,2100.,2500.,5);
+      myLambdaFitSG->SetParameter(0,400.);
+      myLambdaFitSG->SetParameter(1,2286.5);
+      myLambdaFitSG->SetParameter(2, 6);
+      myLambdaFitSG->SetParLimits(2, 0.,20.);
+      myLambdaFitSG->SetParameter(3, 0.);
+      myLambdaFitSG->SetParameter(4, 0.);
+
+      TF1 *LcFitSG1 = new TF1("LcFitSG1",fit2MeV_Gaussian,2100.,2500.,5);
+      LcFitSG1->SetParameter(0,400.);
+      LcFitSG1->SetParameter(1,2286.5);
+      LcFitSG1->SetParameter(2, 6);
+      LcFitSG1->SetParLimits(2, 0.,20.);
+      LcFitSG1->SetParameter(3, 0.);
+      LcFitSG1->SetParameter(4, 0.);
+
+      TF1 *LcFitSG2 = new TF1("LcFitSG2",fit2MeV_Gaussian,2100.,2500.,5);
+      LcFitSG2->SetParameter(0,400.);
+      LcFitSG2->SetParameter(1,2286.5);
+      LcFitSG2->SetParameter(2, 6);
+      LcFitSG2->SetParLimits(2, 0.,20.);
+      LcFitSG2->SetParameter(3, 0.);
+      LcFitSG2->SetParameter(4, 0.);
+
+      TF1 *LcFitSG3 = new TF1("LcFitSG3",fit2MeV_Gaussian,2100.,2500.,5);
+      LcFitSG3->SetParameter(0,400.);
+      LcFitSG3->SetParameter(1,2286.5);
+      LcFitSG3->SetParameter(2, 6);
+      LcFitSG3->SetParLimits(2, 0.,20.);
+      LcFitSG3->SetParameter(3, 0.);
+      LcFitSG3->SetParameter(4, 0.);
+
+      TF1 *LcFitSG4 = new TF1("LcFitSG4",fit2MeV_Gaussian,2100.,2500.,5);
+      LcFitSG4->SetParameter(0,400.);
+      LcFitSG4->SetParameter(1,2286.5);
+      LcFitSG4->SetParameter(2, 6);
+      LcFitSG4->SetParLimits(2, 0.,20.);
+      LcFitSG4->SetParameter(3, 0.);
+      LcFitSG4->SetParameter(4, 0.);
+   
+       TF1 *MagDownFitSG = new TF1("MagDownFitSG",fit2MeV_Gaussian,2100.,2500.,5);
+      MagDownFitSG->SetParameter(0,400.);
+      MagDownFitSG->SetParameter(1,2286.5);
+      MagDownFitSG->SetParameter(2, 6);
+      MagDownFitSG->SetParLimits(2, 0.,20.);
+      MagDownFitSG->SetParameter(3, 0.);
+      MagDownFitSG->SetParameter(4, 0.);
+
+      TF1 *MagUpFitSG = new TF1("MagUpFitSG",fit2MeV_Gaussian,2100.,2500.,5);
+      MagUpFitSG->SetParameter(0,400.);
+      MagUpFitSG->SetParameter(1,2286.5);
+      MagUpFitSG->SetParameter(2, 6);
+      MagUpFitSG->SetParLimits(2, 0.,20.);
+      MagUpFitSG->SetParameter(3, 0.);
+      MagUpFitSG->SetParameter(4, 0.);
+
+      TF1 *ParticleFitSG = new TF1("ParticleFitSG",fit2MeV_Gaussian,2100.,2500.,5);
+      ParticleFitSG->SetParameter(0,400.);
+      ParticleFitSG->SetParameter(1,2286.5);
+      ParticleFitSG->SetParameter(2, 6);
+      ParticleFitSG->SetParLimits(2, 0.,20.);
+      ParticleFitSG->SetParameter(3, 0.);
+      ParticleFitSG->SetParameter(4, 0.);
+
+      TF1 *AntiParticleFitSG = new TF1("AntiParticleFitSG",fit2MeV_Gaussian,2100.,2500.,5);
+      AntiParticleFitSG->SetParameter(0,400.);
+      AntiParticleFitSG->SetParameter(1,2286.5);
+      AntiParticleFitSG->SetParameter(2, 6);
+      AntiParticleFitSG->SetParLimits(2, 0.,20.);
+      AntiParticleFitSG->SetParameter(3, 0.);
+      AntiParticleFitSG->SetParameter(4, 0.);
+
+       LcLowP->Fit("LcFitSG1");
+      LcLowP->SetMinimum(0);
+      c1->Write("Low Lc Momentum Region");
+
+      LcMidLowP->Fit("LcFitSG2");
+      LcMidLowP->SetMinimum(0);
+      c1->Write("MidLow Lc Momentum Region");
+
+      LcMidHighP->Fit("LcFitSG3");
+      LcMidHighP->SetMinimum(0);
+      c1->Write("MidHigh Lc Momentum Region");
+
+      LcHighP->Fit("LcFitSG4");
+      LcHighP->SetMinimum(0);
+      c1->Write("High Lc Momentum Region");
+
+      LcPDistribution->Draw();
+      LcPDistribution->SetMinimum(0);
+      c1->Write("Lc Momentum Distribution");
+
+      double y1 = LcFitSG1->GetParameter(1);
+      double y2 = LcFitSG2->GetParameter(1);
+      double y3 = LcFitSG3->GetParameter(1);
+      double y4 = LcFitSG4->GetParameter(1);
+
+      double yerr1 = LcFitSG1->GetParError(1);
+      double yerr2 = LcFitSG2->GetParError(1);
+      double yerr3 = LcFitSG3->GetParError(1);
+      double yerr4 = LcFitSG4->GetParError(1);
+
+      const Int_t n = 4;
+      Double_t x[n] = {1,2,3,4};
+      Double_t xerr[n] = {0,0,0,0};
+
+      double LcPAvG = ((y1 + y2 + y3 + y4)/n);
+      Double_t y[n] = {y1 - LcPAvG,y2 - LcPAvG,y3 - LcPAvG,y4 - LcPAvG};
+      Double_t yerr[n] = {yerr1,yerr2,yerr3,yerr4};
+      gLcP = new TGraphErrors(n,x,y,xerr,yerr);
+      gLcP->SetMarkerColor(2);
+      gLcP->SetMarkerStyle(20);
+      gLcP->GetXaxis()->SetNdivisions(4);
+      gLcP->GetXaxis()->SetTitle("LambdaC_P Regions");
+      gLcP->GetYaxis()->SetTitle("Measured Mass - Average Mass [MeV]");
+      gLcP->SetTitle("Deviations in LambdaC_MM of Different LambdaC_P Regions");
+      gLcP->Draw("ALP");
+      c1->Write("Lcplus_P Statistics");
+   
+    PolarityMagDown->Fit("MagDownFitSG");
+    PolarityMagDown->SetMinimum(0);
+    c1->Write("PolarityMagDown");
+
+    PolarityMagUp->Fit("MagUpFitSG");
+    PolarityMagUp->SetMinimum(0);
+    c1->Write("PolarityMagUp");
+
+    double p1 = MagDownFitSG->GetParameter(1);
+    double p2 = MagUpFitSG->GetParameter(1);
+
+    double perr1 = MagDownFitSG->GetParError(1);
+    double perr2 = MagUpFitSG->GetParError(1);
+
+      const Int_t m = 2;
+
+    double PolarityAvG = ((p1 + p2)/m);
+    Double_t p[m] = {p1 - PolarityAvG,p2 - PolarityAvG};
+    Double_t perr[m] = {perr1,perr2};
+
+    gPolarity = new TGraphErrors(m,x,p,xerr,perr);
+    gPolarity->SetMarkerColor(2);
+    gPolarity->SetMarkerStyle(20);
+    gPolarity->GetXaxis()->SetNdivisions(2);
+    gPolarity->GetXaxis()->SetTitle("Magnet Orientations");
+    gPolarity->GetYaxis()->SetTitle("Measured Mass - Average Mass [MeV]");
+    gPolarity->SetTitle("Deviations in LambdaC_MM of Different Magnet Orientations");
+    gPolarity->Draw("ALP");
+    c1->Write("Polarity Statistics");
+
+    Particle->Fit("ParticleFitSG");
+    Particle->SetMinimum(0);
+    c1->Write("Baryon");
+
+    AntiParticle->Fit("AntiParticleFitSG");
+    AntiParticle->SetMinimum(0);
+    c1->Write("AntiBaryon");
+
+    double pp1 = ParticleFitSG->GetParameter(1);
+    double pp2 = AntiParticleFitSG->GetParameter(1);
+
+    double pperr1 = ParticleFitSG->GetParError(1);
+    double pperr2 = AntiParticleFitSG->GetParError(1);
+
+    double ParticleAvG = ((pp1 + pp2)/m);
+    Double_t pp[m] = {pp1 - ParticleAvG,pp2 - ParticleAvG};
+    Double_t pperr[m] = {pperr1,pperr2};
+
+    gPID = new TGraphErrors(m,x,pp,xerr,pperr);
+    gPID->SetMarkerColor(2);
+    gPID->SetMarkerStyle(20);
+    gPID->GetXaxis()->SetNdivisions(2);
+    gPID->GetXaxis()->SetTitle("Particle ID");
+    gPID->GetYaxis()->SetTitle("Measured Mass - Average Mass [MeV]");
+    gPID->SetTitle("Deviations in LambdaC_MM of Baryon or AntiBaryon");
+    gPID->Draw("ALP");
+    c1->Write("ParticleStatistics");
+   
         File->Close();
 }
