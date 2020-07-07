a0c65a6dbe522e0f1c214ed268b06ab9102118d1
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-07-07 16:46:04 -0400

    Create ListLFNs.py

---
+++ LHCb/ListLFNs.py
@@ -0,0 +1,13 @@
+#Creates .txt file with list of LFNs for all subjobs of job argument, "ganga ListLFNs.py 73"
+import sys
+jobNumber = int(sys.argv[1])
+length = len(jobs(jobNumber).subjobs)
+file = open('LFNs.txt','w')
+for i in range(0,length):
+    output = jobs(jobNumber).subjobs(i).backend.getOutputDataLFNs()
+    if( output.hasLFNs() == True):
+        s = str(jobs(jobNumber).subjobs(i).outputfiles[0].accessURL())
+        print s
+        file.write(s)
+        file.write('\n')
+file.close()
