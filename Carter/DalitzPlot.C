f9cec0c4642d7b2f4c000e926c6a7792a7fc6d1e
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-07-08 17:13:14 -0400

    Update DalitzPlot.C

--- Carter/DalitzPlot.C
+++ Carter/DalitzPlot.C
@@ -87,6 +87,10 @@ TLorentzVector P(*Proton_PX, *Proton_PY, *Proton_PZ, *Proton_PE);
 TLorentzVector Kp(*Kplus_PX, *Kplus_PY, *Kplus_PZ, *Kplus_PE);
 TLorentzVector Km(*Kminus_PX, *Kminus_PY, *Kminus_PZ, *Kminus_PE);
 
+TLorentzVector PKp();
+TLorentzVector PKm();
+TLorentzVector KpKm();
+   
 PKp = (P + Kp);
 PKm = (P + Km);
 KpKm = (Kp + Km);
