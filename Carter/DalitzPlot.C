6ae42b3f4f935e3b8bff1be31fdaa67aca6e4015
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-07-08 16:44:56 -0400

    Update DalitzPlot.C

--- Carter/DalitzPlot.C
+++ Carter/DalitzPlot.C
@@ -97,21 +97,21 @@ double E_P  = TMath::Sqrt(((P_P)*(P_P))+((M_P)*(M_P)));
 double E_Kp = TMath::Sqrt(((P_Kp)*(P_Kp))+((M_Kp)*(M_Kp)));
 double E_Km = TMath::Sqrt(((P_Km)*(P_Km))+((M_Km)*(M_Km)));
  
-TLorentzVector P(*Proton_PX, *Proton_PY, *Proton_PZ, E_P);
-TLorentzVector Kp(*Kplus_PX, *Kplus_PY, *Kplus_PZ, E_Kp);
-TLorentzVector Km(*Kminus_PX, *Kminus_PY, *Kminus_PZ, E_Km);
+TLorentzVector P(*Proton_PX, *Proton_PY, *Proton_PZ, *Proton_PE);
+TLorentzVector Kp(*Kplus_PX, *Kplus_PY, *Kplus_PZ, *Kplus_PE);
+TLorentzVector Km(*Kminus_PX, *Kminus_PY, *Kminus_PZ, *Kminus_PE);
 
 double ProtonMass = P.Mag();
 double KplusMass  = Kp.Mag();
 double KminusMass = Km.Mag();
 
    //Creating M^2 Variables
-//double M2_KpKm = (KplusMass + KminusMass)*(KplusMass + KminusMass)/(1000*1000);
-//double M2_PKm  = (ProtonMass + KminusMass)*(ProtonMass + KminusMass)/(1000*1000);
-//double M2_PKp  = (KplusMass + ProtonMass)*(KplusMass + ProtonMass)/(1000*1000);
-double M2_KpKm = ((((E_Kp)+(E_Km))*((E_Kp)+(E_Km))) - (((P_Kp)+(P_Km))*((P_Kp)+(P_Km))))/(1000*1000);
-double M2_PKm  = ((((E_P)+(E_Km))*((E_P)+(E_Km))) - (((P_P)+(P_Km))*((P_P)+(P_Km))))/(1000*1000);
-double M2_PKp  = ((((E_P)+(E_Kp))*((E_P)+(E_Kp))) - (((P_P)+(P_Kp))*((P_P)+(P_Kp))))/(1000*1000);
+double M2_KpKm = (KplusMass + KminusMass)*(KplusMass + KminusMass)/(1000*1000);
+double M2_PKm  = (ProtonMass + KminusMass)*(ProtonMass + KminusMass)/(1000*1000);
+double M2_PKp  = (KplusMass + ProtonMass)*(KplusMass + ProtonMass)/(1000*1000);
+//double M2_KpKm = ((((E_Kp)+(E_Km))*((E_Kp)+(E_Km))) - (((P_Kp)+(P_Km))*((P_Kp)+(P_Km))))/(1000*1000);
+//double M2_PKm  = ((((E_P)+(E_Km))*((E_P)+(E_Km))) - (((P_P)+(P_Km))*((P_P)+(P_Km))))/(1000*1000);
+//double M2_PKp  = ((((E_P)+(E_Kp))*((E_P)+(E_Kp))) - (((P_P)+(P_Kp))*((P_P)+(P_Kp))))/(1000*1000);
 
  DalitzPlotUncut->Fill(M2_KpKm, M2_PKm);   
    
