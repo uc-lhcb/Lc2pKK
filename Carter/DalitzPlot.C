357b0d51c2b2c7abb47f59e1e9bf5c153b380ad5
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-07-10 19:55:51 -0400

    Update DalitzPlot.C

--- Carter/DalitzPlot.C
+++ Carter/DalitzPlot.C
@@ -256,7 +256,8 @@ double M2_KpKm = KpKm.Mag2()/(1000*1000);
 bool DalitzCut = (
      (M2_KpKm > 1.025)
   && (M2_KpKm < 1.05672)
-  && (M2_PKm > 2.6)
+  && (M2_PKm > 2.4)
+  && (M2_PKm > 2.4) 
    );
 
    bool LBCut = (
