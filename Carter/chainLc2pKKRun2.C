8e829d0210bf116d160c3a9d98e8c6a930a04dcc
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-07-10 10:28:31 -0400

    Update chainLc2pKKRun2.C

--- Carter/chainLc2pKKRun2.C
+++ Carter/chainLc2pKKRun2.C
@@ -4,5 +4,9 @@
 {
 TChain chain("Lc2pKKTuple/DecayTree");
 chain.Add("/share/lazy/Lc2pKK/Ntuples/Lc2pKK_MagDown2017.root");
-chain.Add("/share/lazy/Lc2pKK/Ntuples/LcMagUp8.root");
+chain.Add("/share/lazy/Lc2pKK/Ntuples/LcMagUp1.root");
+chain.Add("/share/lazy/Lc2pKK/Ntuples/LcMagUp2.root");
+chain.Add("/share/lazy/Lc2pKK/Ntuples/LcMagUp3.root");
+chain.Add("/share/lazy/Lc2pKK/Ntuples/LcMagUp4.root");
+chain.Add("/share/lazy/Lc2pKK/Ntuples/LcMagUp5.root");
 }
