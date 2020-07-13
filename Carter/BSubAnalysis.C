661974ee00135c13ead2f02fcf92e0cba5414f4d
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-07-13 14:01:58 -0400

    Update BSubAnalysis.C

--- Carter/BSubAnalysis.C
+++ Carter/BSubAnalysis.C
@@ -1,15 +1,36 @@
 //Background Subtracting Data To Observe Patterns in ROOT Variables
-//Check PT, IPCHI2_OWNPV, SUMPT, DOCAMAX, DOCAMAXCHI2MAX, & TAU
+//Check PT, IPCHI2_OWNPV, DOCAMAXCHI2MAX, & TAU
 
 #define BSubAnalysis_cxx
 #include "BSubAnalysis.h"
 
-#include <TH2.h>
+#include <TH1D.h>
+#include <TH2D.h>
 #include <TStyle.h>
+#include <TCanvas.h>
+#include <TFile.h>
+#include <TF1.h>
+
+#include "fit1MeV_Gaussian.C"
+#include "fitHalfMeV_Gaussian.C"
+#include "DGOneMuOneTotal1MeV.C"
+#include "DGOneMuOneTotalHalfMeV.C"
+#include "DGTwoMuOneTotal1MeV.C"
+#include "DGTwoMuOneTotalHalfMeV.C"
+
+TH1D * LcPT = nullptr;
+TH1D * KpPT = nullptr;
+TH1D * KmPT = nullptr;
+TH1D * LcChi2 = nullptr;
+TH1D * KpChi2 = nullptr;
+TH1D * KmChi2 = nullptr;
+TH1D * DOCAMax = nullptr;
+TH1D * LcTAU = nullptr;
 
 void BSubAnalysis::Begin(TTree * /*tree*/)
 {
    TString option = GetOption();
+   
 }
 
 void BSubAnalysis::SlaveBegin(TTree * /*tree*/)
