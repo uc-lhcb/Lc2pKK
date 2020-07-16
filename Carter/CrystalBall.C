ec467eb18a122948e2070494068bdfc49827d5bc
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-07-16 13:39:07 -0400

    Update CrystalBall.C

--- Carter/CrystalBall.C
+++ Carter/CrystalBall.C
@@ -5,28 +5,28 @@
 
 Double_t CrystalBall(Double_t *v, Double_t *par) {
 
-// var[0] = x, par[0] = mu, par[1] = sigma, 
-// par[2] = decay constant 'kl', par [3] = decay constant 'kh'
-// par[4] = Linear Background Intercept, par[5] = Linear Background Slope
+// var[0] = x, par[0] = mu, par[1] = sigma, par[2] = signal total
+// par[3] = decay constant 'kl', par [4] = decay constant 'kh'
+// par[5] = Linear Background Intercept, par[6] = Linear Background Slope
 
 Double_t arg = 0;
   if (par[1] != 0)
     arg = (v[0] - par[0])/par[1];
 
  //Low Shoulder Exponential
-  Double_t LEXP = TMath::Exp(0.5*par[2]*par[2] + par[2]*arg);
+  Double_t LEXP = par[2]*TMath::Exp(0.5*par[3]*par[3] + par[3]*arg);
  
 //Gaussian
-Double_t Gauss = TMath::Exp(-0.5*arg*arg);
+Double_t Gauss = par[2]*TMath::Exp(-0.5*arg*arg);
 
 //High Shoulder Exponential
-Double_t HEXP = TMath::Exp(0.5*par[3]*par[3] - par[3]*arg);
+Double_t HEXP = par[2]*TMath::Exp(0.5*par[4]*par[4] - par[4]*arg);
   
 Double_t fitval = 0;
   
-     if (arg <= -(par[2])) fitval = LEXP + par[4] + v[0]*par[5];
-else if (arg > par[3]) fitval = HEXP + par[4] + v[0]*par[5];
-else if (arg > -(par[2]) && arg <= par[3]) fitval = Gauss + par[4] + v[0]*par[5];
+     if (arg <= -(par[3])) fitval = LEXP + par[5] + v[0]*par[6];
+else if (arg > par[4]) fitval = HEXP + par[5] + v[0]*par[6];
+else if (arg > -(par[3]) && arg <= par[4]) fitval = Gauss + par[5] + v[0]*par[6];
 
     return fitval;
 }
