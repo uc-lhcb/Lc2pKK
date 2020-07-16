5c05d6362301631e3a858fe67b7f2de5ebe070c4
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-07-16 14:02:57 -0400

    Update CrystalBall.C

--- Carter/CrystalBall.C
+++ Carter/CrystalBall.C
@@ -7,20 +7,23 @@ Double_t CrystalBall(Double_t *v, Double_t *par) {
 
 // var[0] = x, par[0] = mu, par[1] = sigma, par[2] = signal total
 // par[3] = decay constant 'kl', par [4] = decay constant 'kh'
-// par[5] = Linear Background Intercept, par[6] = Linear Background Slope
+// par[4] = Linear Background Intercept, par[5] = Linear Background Slope
 
 Double_t arg = 0;
   if (par[1] != 0)
     arg = (v[0] - par[0])/par[1];
 
+  //0.5 MeV Bin Width Scaling Factor
+  Double_t Scale = par[2]*(0.5)/(TMath::Sqrt(TMath::TwoPi())*par[1]);
+  
  //Low Shoulder Exponential
-  Double_t LEXP = par[2]*TMath::Exp(0.5*par[3]*par[3] + par[3]*arg);
+  Double_t LEXP = Scale*TMath::Exp(0.5*par[3]*par[3] + par[3]*arg);
  
 //Gaussian
-Double_t Gauss = par[2]*TMath::Exp(-0.5*arg*arg);
+Double_t Gauss = Scale*TMath::Exp(-0.5*arg*arg);
 
 //High Shoulder Exponential
-Double_t HEXP = par[2]*TMath::Exp(0.5*par[4]*par[4] - par[4]*arg);
+Double_t HEXP = Scale*TMath::Exp(0.5*par[4]*par[4] - par[4]*arg);
   
 Double_t fitval = 0;
   
