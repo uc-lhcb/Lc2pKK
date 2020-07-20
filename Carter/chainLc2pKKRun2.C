2342ca36e658220a79f7ae73fc7b11c5548fb6d2
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2020-07-20 10:12:11 -0400

    Update chainLc2pKKRun2.C

--- Carter/chainLc2pKKRun2.C
+++ Carter/chainLc2pKKRun2.C
@@ -5,5 +5,6 @@
 TChain chain("Lc2pKKTuple/DecayTree");
 chain.Add("/share/lazy/Lc2pKK/2017/Lc2pKK_MagDown2017.root");
 chain.Add("/share/lazy/Lc2pKK/2017/Lc2pKK_MagUp2017.root");
-chain.Add("/share/lazy/Lc2pKK/2018/Lc2pKK_MagDown2018.root");
+//chain.Add("/share/lazy/Lc2pKK/2018/Lc2pKK_MagDown2018.root");
+//chain.Add("/share/lazy/Lc2pKK/2018/Lc2pKK_MagUp2018.root");  
 }
