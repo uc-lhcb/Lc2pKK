60ed04359d1fea66108a593a13bc6f58b5814c9d
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2021-06-10 16:22:28 -0400

    Add files via upload

---
+++ nTupleCode/Lc2pKK/2018/MagnetUp2018/GangaTestData2018MagUp.py
@@ -0,0 +1,13 @@
+j = Job(name='Lc+ ganga job - 10 Files of 2018 Data MagUp')
+myApp = GaudiExec()
+myApp.directory = "./DaVinciDev_v45r1"
+j.application = myApp
+j.application.options = ['Davinci2018MagUp.py']
+j.application.platform = 'x86_64-centos7-gcc8-opt'
+bkPath = '/LHCb/Collision18/Beam6500GeV-VeloClosed-MagUp/Real Data/Turbo05/94000000/CHARMCHARGED.MDST'
+data  = BKQuery(bkPath, dqflag=['OK']).getDataset()
+j.inputdata = data[0:10]
+j.backend = Dirac()
+j.splitter = SplitByFiles(filesPerJob=1)
+j.outputfiles = [DiracFile('*.root'), LocalFile('stdout')]
+j.submit()
