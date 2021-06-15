28171e929519fe3ea94907a3b3a32645c1cb8f8f
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2021-06-15 14:16:08 -0400

    Update GangaTestData2016MagUp.py

--- nTupleCode/Lc2pKK/2016/MagnetUp2016/GangaTestData2016MagUp.py
+++ nTupleCode/Lc2pKK/2016/MagnetUp2016/GangaTestData2016MagUp.py
@@ -4,7 +4,7 @@ myApp.directory = "./DaVinciDev"
 j.application = myApp
 j.application.options = ['DaVinci2016MagUp.py']
 j.application.platform = 'x86_64-centos7-gcc8-opt'
-bkPath = '/LHCb/Collision16/Beam6500GeV-VeloClosed-MagUp/Real Data/Turbo03/94000000/CHARMCHARGED.MDST'
+bkPath = '/LHCb/Collision16/Beam6500GeV-VeloClosed-MagUp/Real Data/Turbo03a/94000000/CHARMCHARGED.MDST'
 data  = BKQuery(bkPath, dqflag=['OK']).getDataset()
 j.inputdata = data[0:10]
 j.backend = Dirac()
