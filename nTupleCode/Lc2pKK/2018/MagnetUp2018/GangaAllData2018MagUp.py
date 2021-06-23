2788b73ccad8a76d59386020d6803bcc1f20f729
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2021-06-23 16:03:37 -0400

    Update GangaAllData2018MagUp.py

--- nTupleCode/Lc2pKK/2018/MagnetUp2018/GangaAllData2018MagUp.py
+++ nTupleCode/Lc2pKK/2018/MagnetUp2018/GangaAllData2018MagUp.py
@@ -2,7 +2,7 @@ j = Job(name='Lc+ ganga job - All 2018 Data MagUp')
 myApp = GaudiExec()
 myApp.directory = "./DaVinciDev"
 j.application = myApp
-j.application.options = ['Davinci2018MagUp.py']
+j.application.options = ['DaVinci2018MagUp.py']
 j.application.platform = 'x86_64-centos7-gcc8-opt'
 bkPath = '/LHCb/Collision18/Beam6500GeV-VeloClosed-MagUp/Real Data/Turbo05/94000000/CHARMCHARGED.MDST'
 data  = BKQuery(bkPath, dqflag=['OK']).getDataset()
