7415982df0a566321f2c0ce39e7dd6242daf204c
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2021-07-19 14:27:39 -0400

    Add files via upload

---
+++ LHCb/2018/MagnetDown2018/GangaAllData2018MagDown.py
@@ -0,0 +1,13 @@
+j = Job(name='Lc+ ganga job - All 2018 Data MagDown')
+myApp = GaudiExec()
+myApp.directory = "./DaVinciDev"
+j.application = myApp
+j.application.options = ['DaVinci2018MagDown.py']
+j.application.platform = 'x86_64-centos7-gcc8-opt'
+bkPath = '/LHCb/Collision18/Beam6500GeV-VeloClosed-MagDown/Real Data/Turbo05/94000000/CHARMCHARGED.MDST'
+data  = BKQuery(bkPath, dqflag=['OK']).getDataset()
+j.inputdata = data
+j.backend = Dirac()
+j.splitter = SplitByFiles(filesPerJob=5, ignoremissing = True)
+j.outputfiles = [DiracFile('*.root'), LocalFile('stdout')]
+j.submit()
