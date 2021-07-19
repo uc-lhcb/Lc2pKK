ec2444f218b9cbe240f25e9d6d627eb68b4d7c8f
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2021-07-19 14:28:20 -0400

    Delete Analysis directory

--- Analysis/PIDSelection/chain.C
+++
@@ -1,5 +0,0 @@
-{
-TChain chain("Lc2pKKTuple/DecayTree");
-chain.Add("/share/lazy/Lc2pKK/2017Corrected/Lc2pKK_MagDown2017Corrected.root");
-chain.Add("/share/lazy/Lc2pKK/2017Corrected/Lc2pKK_MagUp2017Corrected.root");
-}
