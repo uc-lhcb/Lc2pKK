53070eadb706d4fcf58e01298e2be3e4c4d419fb
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2021-07-19 15:42:51 -0400

    Add files via upload

---
+++ MomentumCorrections/DifferentCorrections/mKSq243717_mDpSq3495609/chain.C
@@ -0,0 +1,5 @@
+{
+TChain chain("Lc2pKKTuple/DecayTree");
+chain.Add("/share/lazy/Lc2pKK/2017Corrected/Lc2pKK_MagDown2017Corrected.root");
+chain.Add("/share/lazy/Lc2pKK/2017Corrected/Lc2pKK_MagUp2017Corrected.root");
+}
