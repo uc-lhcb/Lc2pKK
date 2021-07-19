9263d6ab7cd36d5a34bba28d0818fc9e8aa123bb
Carter Eikenbary <51170046+Eikenbcr@users.noreply.github.com>
2021-07-19 15:27:30 -0400

    Add files via upload

---
+++ MomentumCorrections/DifferentFits/CrystalBall_Linear/chain.C
@@ -0,0 +1,5 @@
+{
+TChain chain("Lc2pKKTuple/DecayTree");
+chain.Add("/Users/eikenbcr/Desktop/Research/Lc2pKKAnalysis/Lc2pKK_MagDown2017Corrected.root");
+chain.Add("/Users/eikenbcr/Desktop/Research/Lc2pKKAnalysis/Lc2pKK_MagUp2017Corrected.root");
+}
